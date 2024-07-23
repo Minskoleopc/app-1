b69ed7911631eb6dc8401d8fab65afbdb0ffe31c
unknown <Minskoleopc>
2024-07-23 19:16:40 +0530

    added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import App2 from './App2';
-
+import Functionone from './Functionone';
+import Functiontwo from './Functiontwo';
+import Functionthree from './Functionthree';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <App2/>
+    {/* <App />
+    <App2/> */}
+    {/* <Functionone></Functionone> */}
+    {/* <Functiontwo></Functiontwo> */}
+    <Functionthree></Functionthree>
   </React.StrictMode>
 );
 
